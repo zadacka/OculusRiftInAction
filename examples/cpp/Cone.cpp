0dbbe2f459555acf35295593e38372d3998a9103
Alex Zawadzki <alexander.zawadzki@gmail.com>
2014-07-31 16:29:47 +0100

    Add VTK into the CMakeLists.txt so that it links

---
+++ examples/cpp/Cone.cpp
@@ -0,0 +1,52 @@
+
+// not using CMake, so the following defines are needed:
+#define vtkRenderingCore_AUTOINIT 4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)
+#define vtkRenderingVolume_AUTOINIT 1(vtkRenderingVolumeOpenGL)
+
+#include "vtkConeSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderWindow.h"
+#include "vtkCamera.h"
+#include "vtkActor.h"
+#include "vtkRenderer.h"
+
+int main()
+{
+  vtkConeSource *cone = vtkConeSource::New();
+  cone->SetHeight( 3.0 );
+  cone->SetRadius( 1.0 );
+  cone->SetResolution( 10 );
+
+  vtkPolyDataMapper *coneMapper = vtkPolyDataMapper::New();
+  coneMapper->SetInputConnection( cone->GetOutputPort() );
+
+  vtkActor *coneActor = vtkActor::New();
+  coneActor->SetMapper( coneMapper );
+
+  vtkRenderer *ren1= vtkRenderer::New();
+  ren1->AddActor( coneActor );
+  ren1->SetBackground( 0.1, 0.2, 0.4 );
+
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+  renWin->AddRenderer( ren1 );
+  renWin->SetSize( 1280, 800 );
+
+  int i;
+  for (i = 0; i < 360; ++i)
+    {
+    // render the image
+    renWin->Render();
+    // rotate the active camera by one degree
+    ren1->GetActiveCamera()->Azimuth( 1 );
+    }
+
+  cone->Delete();
+  coneMapper->Delete();
+  coneActor->Delete();
+  ren1->Delete();
+  renWin->Delete();
+
+  return 0;
+}
+
+
